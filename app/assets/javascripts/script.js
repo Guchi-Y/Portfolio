0c412926cb78c1b8d24385b4eddb6362d3a97c73
Guchi-Y <i25mas25@gmail.com>
2019-10-21 19:27:58 +0900

    Write javascript for header display and scrolling

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,50 @@
+$(function(){
+	
+	var
+	  winW = $(window).width(),
+		nav = $('#mainnav ul a');
+	
+	if (winW < 880){
+		var headerH = 0;
+	}
+	else{
+		var headerH = 63;
+	}
+	
+	$(nav).on('click', function(){
+		nav.removeClass('active');
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		$(this).addClass('active');
+		if (winW < 880){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+
+	
+	$('.panel').hide();
+	$('#menuWrap').toggle(function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').toggleClass('close');
+	},
+	function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').removeClass('close');
+	});
+		
+	$(window).on('scroll', function(){
+		var curPos = $(this).scrollTop();
+		if(curPos > 80){
+			$('#mainnav').addClass('changeNav');
+		}
+		else{
+			$('#mainnav').removeClass('changeNav');
+		}
+	});
+	
+});
